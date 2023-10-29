21cf6849419c2051686f874c21494a7edab09843
Stefeson <stefesongomesdealmeidanetoo@gmail.com>
2023-10-29 10:53:19 -0300

    Multiplo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Stefeson
+
+Console.Clear();
+
+int numero1, numero2;
+
+Console.WriteLine("--- Multiplo ---");
+
+Console.Write("Digite um número: ");
+numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Avaliar se é múltiplo de: ");
+numero2 = Convert.ToInt32(Console.ReadLine());
+
+if (numero1 % numero2 == 0)
+{
+    Console.WriteLine($"{numero1} é multiplo de {numero2}");
+}
+else
+{
+    Console.WriteLine($"{numero1} não é multiplo de {numero2}");
+}
